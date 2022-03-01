b7ef394ac6c736c4315843cbfa4f0d979c2c666e
wslee3 <leemuo123@gmail.com>
2022-03-01 18:38:29 +0900

    [feature] 서버 mysql config 작성 및 관렴 함수 작성

--- back/src/config/sql.ts
+++ back/src/config/sql.ts
@@ -1,1 +1,6 @@
-// DB Connection 정보
+export const dbConfig = {
+  host: process.env.BN_DB_HOST,
+  user: process.env.BN_DB_USER,
+  password: process.env.BN_DB_PW,
+  db: process.env.BN_DB_DB,
+};
--- back/src/library/sql.ts
+++ back/src/library/sql.ts
@@ -1,1 +1,31 @@
-// sql 관련 함수
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.BN_DB_HOST,
+  user: process.env.BN_DB_USER,
+  password: process.env.BN_DB_PW,
+  database: process.env.BN_DB_DB,
+  charset: "utf8",
+  multipleStatements: false,
+  timezone: "UTC",
+});
+
+const connect = (func: any) => async () => {
+  const conn = await pool.getConnection();
+  try {
+    const result = await func(conn);
+    conn.release();
+    return result;
+  } catch (error) {
+    conn.release();
+    throw error;
+  }
+};
+
+export const sqlConn = (func: any) => connect(func)();
+
+export const execQuery = async (conn: mysql.PoolConnection, sql: string) => {
+  // 쿼리 실행 전 실행해야할 쿼리가 있다면 여기 작성
+  return conn.query(sql);
+};
