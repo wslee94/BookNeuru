8eded66f71953b4bbbc74f48d7a893d78b1a78a1
wslee3 <leemuo123@gmail.com>
2022-03-09 19:04:35 +0900

    [feature] back Response Class 정의 및 사용

--- back/src/api/user/user.ts
+++ back/src/api/user/user.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import mysql from "mysql2/promise";
 import { apiWithNoToken } from "library/api";
+import Response from "library/response";
 import * as userQuery from "./userQuery";
 
 const signup = apiWithNoToken(async (conn: mysql.PoolConnection, param: any) => {
-  const result = await userQuery.insertUser(conn, param);
+  await userQuery.insertUser(conn, param);
 
   // insert하기 전 동일 email 체크하기
   // 동일 이메일이 있으면 status FAIL 처리
   // return 되는 data 형식 정의 e.g. { status: 'ERROR' | 'FAILE' | 'SUCCESS', data: {...}, message: '~~~'" }
 
-  return result;
+  return new Response("SUCCESS", null, "");
 });
 
 export default express.Router().post("", signup);
---
+++ back/src/library/response.ts
@@ -0,0 +1,15 @@
+type statusType = "ERROR" | "FAILE" | "SUCCESS";
+type dataType = object | null;
+type messageType = string;
+
+export default class Response {
+  status: statusType;
+  data: dataType;
+  message: messageType;
+
+  constructor(status: statusType, data: dataType, message: messageType) {
+    this.status = status;
+    this.data = data;
+    this.message = message;
+  }
+}
