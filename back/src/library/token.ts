8eba1049db6fb4dd55df597e91a8db85fc8beedb
wslee3 <leemuo123@gmail.com>
2022-03-11 21:05:35 +0900

    [feature] jwt 관련 함수 작성

---
+++ back/src/library/token.ts
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { Request } from "express";
+import { jwtSecret } from "config/token";
+
+export const generateToken = (userInfo: { userID: number; email: string; name: string }) => {
+  const token = jwt.sign(userInfo, jwtSecret, { expiresIn: "7d" });
+  return token;
+};
+
+export const checkToken = (token: string) => {
+  if (!token) return { status: "FAIL", token: null, user: null };
+  try {
+    const userInfo = jwt.verify(token, jwtSecret);
+    return { status: "SUCCESS", token: null, user: userInfo };
+  } catch (error) {
+    return { status: "FAIL", token, user: null };
+  }
+};
+
+export const getToken = (req: Request) => {
+  if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+    return req.headers.authorization.split(" ")[1];
+  }
+  return null;
+};
