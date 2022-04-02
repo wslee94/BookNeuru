46622c382ec5686d09ee9c4d94941c76cc1706fa
wslee3 <leemuo123@gmail.com>
2022-04-02 16:23:35 +0900

    [fix] front meeting 사용자 초대 시 validation check

--- front/src/app/routes/web/meetingInfo/invitation.tsx
+++ front/src/app/routes/web/meetingInfo/invitation.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Field from 'components/web/Field';
 import InputBox from 'components/web/InputBox';
 import Button from 'components/web/Button';
+import Alert from 'components/web/Alert';
+import * as func from 'helpers/func';
 import { apiCall, getAjaxData } from 'helpers/ajax';
 import { handleAjaxError } from 'helpers/error';
 import { meetingInfoTypes } from './types';
@@ -13,6 +15,7 @@ interface InvitationProps {
 
 function Invitation({ closeDialog, meetingInfo }: InvitationProps) {
   const [email, setEmail] = useState('');
+  const [alert, setAlert] = useState({ isOpen: false, title: '', text: '' });
 
   const invite = async () => {
     try {
@@ -32,6 +35,20 @@ function Invitation({ closeDialog, meetingInfo }: InvitationProps) {
     }
   };
 
+  const checkValidation = () => {
+    if (func.checkBlank(email)) {
+      setAlert({ isOpen: true, title: '필수 값을 입력해 주세요.', text: '이메일을 입력해 주세요.' });
+      return false;
+    }
+
+    if (!func.checkEmail(email)) {
+      setAlert({ isOpen: true, title: '입력한 데이터를 확인해 주세요', text: '올바른 이메일 형식으로 입력해 주세요.' });
+      return false;
+    }
+
+    return true;
+  };
+
   return (
     <>
       <div>
@@ -40,9 +57,23 @@ function Invitation({ closeDialog, meetingInfo }: InvitationProps) {
         </Field>
       </div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <Button size="large" label="초대" onClick={invite} style={{ margin: '5px' }} />
+        <Button
+          size="large"
+          label="초대"
+          onClick={() => {
+            if (!checkValidation()) return;
+            invite();
+          }}
+          style={{ margin: '5px' }}
+        />
         <Button type="inverted" size="large" label="닫기" onClick={() => closeDialog()} style={{ margin: '5px' }} />
       </div>
+      <Alert
+        isOpen={alert.isOpen}
+        handleClose={() => setAlert({ ...alert, isOpen: false })}
+        title={alert.title}
+        text={alert.text}
+      />
     </>
   );
 }
